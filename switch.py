a778b30548c6756bbbe65ab8740d75ccd57fc3fc
Vaibhaw <vanilkumar@infoblox.com>
2021-12-14 22:46:34 +0530

    Update switch.py
    update

--- switch.py
+++ switch.py
@@ -9,7 +9,7 @@ password = ""
 for i in HOST:
     tn = telnetlib.Telnet(i.strip())
     tn.write(b"\n")
-    tn.read_until(b"Username: ")
+    tn.read_until(b"login: ")
     usr=f"{user}"
     tn.write(usr.encode('ascii')+b"\n") 
     tn.read_until(b"Password: ")
@@ -17,11 +17,8 @@ for i in HOST:
     tn.write(new.encode('ascii')+b"\n") 
     tn.write(b"\n")
     tn.write(b"copy running-config startup-config\n\n")
-    tn.write(b"copy running-config tftp: \n")
-    tn.write(b"10.197.1.120\n")
-    name=f"{i}"
-    tn.write(name.encode('ascii')+b"\n") 
-    tn.write(b"copy running-config startup-config\n")
+    cm=f"copy running-config tftp://10.197.1.120/{i}\n"
+    tn.write(vm.encode('ascii')+b"\n") 
     tn.write(b"\n")
     tn.write(b"exit\n")
     print(tn.read_all().decode('ascii'))
